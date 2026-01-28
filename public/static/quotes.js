3a833c075f1d2230210097486806924f040d1c90
SYD-watanabe <genspark_dev@genspark.ai>
2026-01-28 10:55:41 +0000

    修正: 作成日のタイムスタンプを日本時間（JST）に対応

--- public/static/quotes.js
+++ public/static/quotes.js
@@ -316,7 +316,8 @@ function formatDate(dateString) {
     month: '2-digit',
     day: '2-digit',
     hour: '2-digit',
-    minute: '2-digit'
+    minute: '2-digit',
+    timeZone: 'Asia/Tokyo'
   })
 }
 
